6f13963d0b938fce075597a7b0e0dbdc2472fdb7
Steven Tohme <steventohme59@gmail.com>
2023-05-21 02:02:18 -0400

    add city class

---
+++ Assignment 1/part_2.py
@@ -0,0 +1,12 @@
+# Simplifies the storage of city instances
+class City:
+    # Constructor
+    def __init__(self, name, description, latitude, longitude):
+        self.name = name
+        self.description = description
+        self.latitude = latitude
+        self.longitude = longitude
+
+
+
+    
