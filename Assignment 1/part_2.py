3acc84e3f0e74436897141562e8c36e66f48cff4
Steven Tohme <steventohme59@gmail.com>
2023-05-21 02:04:23 -0400

    update city docstring

--- Assignment 1/part_2.py
+++ Assignment 1/part_2.py
@@ -1,5 +1,19 @@
-# Simplifies the storage of city instances
+# Author: Steven Tohme
+# Class: CP468 - Artificial Intelligence
+
 class City:
+    """
+    a class to represent a city instance
+
+    ----------
+    Attributes:
+    ----------
+    name (string): The name of the state
+    description (string): The name of the city
+    latitude (float): The latitude of the city
+    longitude (float): The longitude of the city
+
+    """
     # Constructor
     def __init__(self, name, description, latitude, longitude):
         self.name = name
