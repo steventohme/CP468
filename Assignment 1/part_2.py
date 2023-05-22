37bcf86184f7b713519a7e80a6569bb19f8c1fe1
Steven Tohme <steventohme59@gmail.com>
2023-05-21 20:09:37 -0400

    update docstrings

--- Assignment 1/part_2.py
+++ Assignment 1/part_2.py
@@ -10,18 +10,28 @@ class City:
     ----------
     Attributes:
     ----------
-    name (string): The name of the state
-    description (string): The name of the city
-    latitude (float): The latitude of the city
-    longitude (float): The longitude of the city
+        name (string): The name of the state
+        description (string): The name of the city
+        latitude (float): The latitude of the city
+        longitude (float): The longitude of the city
 
     ----------
     Methods:
     ----------
-    calcDistance(city: City) -> float: Calculates the Euclidian distance between the current city and the city passed as a parameter
+        calcDistance(city: City) -> float: Calculates the Euclidian distance between the current city and the city passed as a parameter
 
     """
     def __init__(self, name: str, description: str, latitude: float, longitude: float):
+        """
+        Constructs city object
+
+        Parameters:
+        ----------
+            name (string): The name of the state
+            description (string): The name of the city
+            latitude (float): The latitude of the city
+            longitude (float): The longitude of the city
+        """
         self.name = name
         self.description = description
         self.latitude = latitude
@@ -34,11 +44,11 @@ class City:
 
         Parameters:
         ----------
-        city (City): The city to calculate the distance to
+            city (City): The city to calculate the distance to
 
         Returns:
         ----------
-        distance (float): The distance between the two cities
+            distance (float): The distance between the two cities
         """
         return ((self.latitude - city.latitude)**2 + (self.longitude - city.longitude)**2)**0.5
  
@@ -49,7 +59,7 @@ class Graph:
     ----------
     Attributes:
     ----------
-    graph (dict): A dictionary containing the graph in the form {City: [City1, City2, ...]}
+        graph (dict): A dictionary containing the graph in the form {City: [City1, City2, ...]}
     """
     def __init__(self):
         self.graph = defaultdict(list)
