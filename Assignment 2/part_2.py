f57211d634b4302031d7c53b7cfbdb027201508d
Steven Tohme <steventohme59@gmail.com>
2023-06-16 19:50:55 -0400

    add board str method

--- Assignment 2/part_2.py
+++ Assignment 2/part_2.py
@@ -13,3 +13,21 @@ class Board:
         """
         self.N = N
         self.board = [[0 for i in range(N)] for j in range(N)]
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the board
+
+        Returns:
+        ----------
+            string (str): The string representation of the board
+        """
+        string = ""
+        for row in self.board:
+            string += str(row) + "\n"
+        return string
+
+
+if __name__ == "__main__":
+    board = Board(8)
+    print(board)
