776b614f5950fc3b3a12be123af1b57d55b1baf8
Steven Tohme <steventohme59@gmail.com>
2023-06-29 12:41:53 -0400

    create point class

--- Assignment 3/part_2.py
+++ Assignment 3/part_2.py
@@ -1,8 +1,9 @@
-class point:
+class Point:
     """
     A class to represent a point in 2D space
+
     """
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float) -> None:
         """
         Constructor for point class
 
@@ -11,13 +12,23 @@ class point:
             x (int): The x coordinate of the point
             y (int): The y coordinate of the point
         
-        Attributes:
-        ----------
-            x (int): The x coordinate of the point
-            y (int): The y coordinate of the point
         """
         self.x = x
         self.y = y
+    
+    def calcDistance(self, other: 'Point') -> float:
+        """
+        Calculates the Euclidian distance between the current point and the point passed as a parameter
+
+        Parameters:
+        ----------
+            other (Point): The point to compare to
+        
+        Returns:
+        ----------
+            float: The Euclidian distance between the two points
+        """
+        return ((self.x - other.x)**2 + (self.y - other.y)**2)**0.5
 
 class kMeans:
     def __init__(self, k, data):
